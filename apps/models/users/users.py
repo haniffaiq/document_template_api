5645bb7d02fdc533df7029be75c4dc55bdfc0f6e
hanifFaiq <haniffaiq95@gmail.com>
2024-06-28 15:27:53 +0700

    Update code

--- apps/models/users/users.py
+++ apps/models/users/users.py
@@ -1,7 +1,18 @@
 import uuid
 from datetime import datetime
 from db import db
-from .role import Role  # Import model Role
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    __table_args__ = {'schema': 'authentication'}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    is_deleted = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f"<Role(id={self.id}, name='{self.name}', is_deleted={self.is_deleted})>"
 
 class User(db.Model):
     __tablename__ = 'users'
